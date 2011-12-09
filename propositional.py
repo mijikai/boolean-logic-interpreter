2d9f574211af5a4c1bf16118bb7eae827c1742ff
Lester B <tkhdlstfl.l+git@gmail.com>
2011-12-09 09:25:49 +0800

    Delete tokenize function

--- propositional.py
+++ propositional.py
@@ -97,72 +97,10 @@ class Tokenizer(object):
         self.ind -= len(tok)
         return tok
 
+
 class BoolExpr(object):
-    def __init__(self):
+   def __init__(self):
         pass
-
-"""def parser(string):
-    if type(string) != str:
-        raise ValueError('string must be of type str.')
-
-    tok_iter = Tokenizer(string, OPER)"""
-def tokenize(string):
-    tokens = []
-    stack = []
-    ind = 0
-    length = len(string)
-
-    def consume(oper):
-        nonlocal ind
-        i = 0
-        
-        for charstr, charoper in zip(string[ind:ind + len(oper)].upper(), oper):
-            if charstr != charoper:
-                raise SyntaxError
-            i += 1
-        if i != len(oper):
-            raise SyntaxError
-
-        tokens.append(oper)
-        ind += len(oper) - 1
-    
-    while ind < length:
-        char = string[ind].upper()
-
-        if char in "TF":
-            tokens.append(char)
-        elif char == 'N':
-            consume('NOT')
-        elif char == 'A':
-            consume('AND')
-        elif char == 'O':
-            consume('OR')
-        elif char == 'I':
-            try:
-                consume('IFF')
-            except SyntaxError:
-                consume('IMPLIES')
-        elif char == 'X':
-            consume('XOR')
-        elif char == '(':
-            child = []
-            tokens.append(child)
-            stack.append(tokens)
-            tokens = child
-        elif char == ')':
-            if not stack:
-                raise SyntaxError
-            tokens = stack.pop()
-        elif char in " \t\n\r":
-            pass
-        else:
-            raise SyntaxError
-
-        ind += 1
-
-    if stack:
-        raise SyntaxError
-    return tokens
 
 
 def parse(string):
@@ -185,7 +123,6 @@ def parse(string):
                 while stk_oper:
                     oper = stk_oper.pop()
                     stk_post.append(oper)
-
             else:
                 stk_post.append(tok)
         elif tok in UNARY:

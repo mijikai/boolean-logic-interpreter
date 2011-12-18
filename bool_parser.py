d17ad2befefb3dacf5fcd48122f60d72945e7eb4
Lester B <tkhdlstfl.l+git@gmail.com>
2011-12-18 22:18:44 +0800

    Add parse method and refactor code
    Loop the operator and its precedence instead handwire it.

--- bool_parser.py
+++ bool_parser.py
@@ -1,42 +1,84 @@
 #!/usr/bin/env python3
 
-import pyparsing
-import boolean
+from pyparsing import Word, Literal, Forward, OneOrMore, ZeroOrMore,\
+    Or, MatchFirst, Group, StringEnd
+from boolean import UNARY, BINARY, OPERATORS, PRECEDENCE, CONSTANTS
+from expression import Expression
 
+__all__ = ['parse']
+
+#This function will be called by the pattern when an expression is
+#parsed. We only need the first element of tok and push them in the
+#stack. If the function will just only append the first element of tok
+#to the stack, the stack will appear to be in infix order.
+stack = []
+def toExpression(s, loc, tok):
+    curr = tok[0]
+    if curr in BINARY:
+        args = []
+        for i in range(2):
+            args.append(stack.pop())
+        args.reverse()
+        stack.append(Expression(curr, *args))
+    elif curr in UNARY:
+        stack.append(Expression(curr, stack.pop()))
+    else:
+        stack.append(curr)
+
+#for variables
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 alpha += alpha.upper()
 
-#remove literal true and false
 alpha_list = list(alpha)
-for i in boolean.LITERALS:
+for i in CONSTANTS:
     alpha_list.remove(i)
 alpha = ''.join(alpha_list)
 
-lpar = pyparsing.Literal('(')
-rpar = pyparsing.Literal(')')
+#nesting
+lpar = Literal('(')
+rpar = Literal(')')
 
-#operator literals
-not_ = pyparsing.Literal(boolean.NOT)
-and_ = pyparsing.Literal(boolean.AND)
-or_ = pyparsing.Literal(boolean.OR)
-xor = pyparsing.Literal(boolean.XOR)
-if_ = pyparsing.Literal(boolean.IF)
-iff = pyparsing.Literal(boolean.IFF)
-
-expr = pyparsing.Forward()
-negation = pyparsing.Forward()
-
-var = pyparsing.Word(alpha)
-constant = (pyparsing.Literal(boolean.TRUE) |
-        pyparsing.Literal(boolean.FALSE))
+var = Word(alpha)
+constant = MatchFirst([Literal(i) for i in CONSTANTS])
 operand = constant | var
 
-atom = operand | lpar + expr + rpar
-negation << (not_ + atom | not_ + negation)
-conjunction = (atom | negation) + pyparsing.ZeroOrMore(and_ + (atom | negation))
-disjunction = conjunction + pyparsing.ZeroOrMore(or_ + conjunction)
-ex_disjunction = disjunction + pyparsing.ZeroOrMore(xor + disjunction)
-conditional = ex_disjunction + pyparsing.ZeroOrMore(if_ + ex_disjunction)
-expr << conditional + pyparsing.ZeroOrMore(iff + conditional)
+oper_literals = {oper:Literal(oper) for oper in OPERATORS}
+oper_patterns = {oper:Forward() for oper in OPERATORS}
 
-pattern = expr + pyparsing.StringEnd()
+expr = Forward()
+atom = operand | lpar + expr + rpar
+
+prev_pattern = atom
+
+atom.setParseAction(toExpression) 
+
+for opers in PRECEDENCE:
+    pattern_list = []
+    for op in opers:
+        if op in UNARY:
+            alternative = OneOrMore(oper_literals[op]) + prev_pattern
+            alternative.setParseAction(toExpression) 
+            unary_pattern = prev_pattern | alternative
+            pattern_list.append(unary_pattern)
+        elif op in BINARY:
+            rest = oper_literals[op] + prev_pattern
+            rest.setParseAction(toExpression)
+            binary_pattern = prev_pattern + ZeroOrMore(rest)
+            pattern_list.append(binary_pattern)
+        else:
+            raise Exception(op)
+
+    if len(pattern_list) == 1: #Do not create another object
+        prev_pattern = pattern_list[0]
+    else:
+        prev_pattern = Or(pattern_list)
+
+expr << prev_pattern
+pattern = expr + StringEnd()
+
+def parse(string):
+    """Parses the string into an Expression object."""
+    global stack
+    stack = []
+    pattern.parseString(string) 
+    return stack.pop() #after parsing the stack should have only one item
